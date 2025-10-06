8cf633088a9bb6816df4ae7be718d55cc1f38b54
quentindhr <124066086+quentindhr@users.noreply.github.com>
2025-10-06 16:01:58 +0200

    Force a toi

--- skeleton-web-app-school-main/front-skeleton/src/app/home/home.component.ts
+++ skeleton-web-app-school-main/front-skeleton/src/app/home/home.component.ts
@@ -34,9 +34,47 @@ export class HomeComponent {
     { date: new Date('2025-12-14'), domicile: 'Toulouse FC', exterieur: 'FC Lorient', lieu: 'Stadium de Toulouse' },
   ];
 
+  // DONNÉES DU CLASSEMENT
+  // Note: 'pos' est la clé pour la position, utilisée pour le tri et le filtrage.
+  classementLigue1 = [
+    { pos: 1, club: 'Paris Saint-Germain', pts: 16, j: 7, g: 5, n: 1, p: 1, diff: '+8', logo: 'https://upload.wikimedia.org/wikipedia/fr/4/44/Logo_du_Paris_Saint-Germain.svg' },
+    { pos: 2, club: 'Olympique de Marseille', pts: 15, j: 7, g: 5, n: 0, p: 2, diff: '+10', logo: 'https://upload.wikimedia.org/wikipedia/fr/7/75/Logo_Olympique_Marseille_2017.svg' },
+    { pos: 3, club: 'RC Strasbourg Alsace', pts: 15, j: 7, g: 5, n: 0, p: 2, diff: '+7', logo: 'https://upload.wikimedia.org/wikipedia/fr/b/b3/Logo_RC_Strasbourg_2020.svg' },
+    { pos: 4, club: 'Olympique Lyonnais', pts: 15, j: 7, g: 5, n: 0, p: 2, diff: '+4', logo: 'https://upload.wikimedia.org/wikipedia/fr/4/4e/Logo_Olympique_Lyonnais_2022.svg' },
+    { pos: 5, club: 'AS Monaco', pts: 13, j: 7, g: 4, n: 1, p: 2, diff: '+4', logo: 'https://upload.wikimedia.org/wikipedia/fr/d/da/Logo_AS_Monaco_2013.svg' },
+    { pos: 6, club: 'RC Lens', pts: 13, j: 7, g: 4, n: 1, p: 2, diff: '+4', logo: 'https://upload.wikimedia.org/wikipedia/fr/4/4b/Logo_RC_Lens_2020.svg' },
+    { pos: 7, club: 'LOSC Lille', pts: 11, j: 7, g: 3, n: 2, p: 2, diff: '+4', logo: 'https://upload.wikimedia.org/wikipedia/fr/e/ed/Logo_LOSC_Lille_2018.svg' },
+    { pos: 8, club: 'Paris FC', pts: 10, j: 7, g: 3, n: 1, p: 3, diff: '-1', logo: 'https://upload.wikimedia.org/wikipedia/fr/f/fd/Logo_Paris_FC.svg' },
+    { pos: 9, club: 'Toulouse FC', pts: 10, j: 7, g: 3, n: 1, p: 3, diff: '-1', logo: 'https://upload.wikimedia.org/wikipedia/fr/5/5e/Logo_Toulouse_FC_2018.svg' },
+    { pos: 10, club: 'Stade Rennais FC', pts: 10, j: 7, g: 2, n: 4, p: 1, diff: '-1', logo: 'https://upload.wikimedia.org/wikipedia/fr/e/ec/Logo_Stade_Rennais_FC_2020.svg' },
+    { pos: 11, club: 'Stade Brestois 29', pts: 8, j: 7, g: 2, n: 2, p: 3, diff: '0', logo: 'https://upload.wikimedia.org/wikipedia/fr/4/42/Logo_Stade_Brestois_29.svg' },
+    { pos: 12, club: 'OGC Nice', pts: 8, j: 7, g: 2, n: 2, p: 3, diff: '-3', logo: 'https://upload.wikimedia.org/wikipedia/fr/5/52/Logo_OGC_Nice_2013.svg' },
+    { pos: 13, club: 'FC Lorient', pts: 7, j: 7, g: 2, n: 1, p: 4, diff: '-7', logo: 'https://upload.wikimedia.org/wikipedia/fr/2/29/Logo_FC_Lorient.svg', isHome: true },
+    { pos: 14, club: 'Le Havre AC', pts: 6, j: 7, g: 1, n: 3, p: 3, diff: '-2', logo: 'https://upload.wikimedia.org/wikipedia/fr/5/56/Logo_Le_Havre_AC_-_2022.svg' },
+    { pos: 15, club: 'FC Nantes', pts: 6, j: 7, g: 1, n: 3, p: 3, diff: '-2', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Logo_FC_Nantes_2019.svg' },
+    { pos: 16, club: 'AJ Auxerre', pts: 6, j: 7, g: 2, n: 0, p: 5, diff: '-5', logo: 'https://upload.wikimedia.org/wikipedia/fr/c/cd/Logo_AJ_Auxerre_2023.svg' },
+    { pos: 17, club: 'Angers SCO', pts: 5, j: 7, g: 1, n: 2, p: 4, diff: '-8', logo: 'https://upload.wikimedia.org/wikipedia/fr/d/d3/Logo_Angers_SCO_-_2021.svg' },
+    { pos: 18, club: 'FC Metz', pts: 2, j: 7, g: 0, n: 2, p: 5, diff: '-11', logo: 'https://upload.wikimedia.org/wikipedia/fr/a/a2/Logo_FC_Metz_2022.svg' },
+  ];
+
+  // FONCTION POUR LE CLASSEMENT CIBLÉ
+  get getFocusedClassement() {
+    // 1. Trouver la position (index) du FC Lorient dans le tableau
+    const lorientIndex = this.classementLigue1.findIndex(club => club.club === 'FC Lorient');
+
+    // 2. Déterminer les indices de début et de fin pour le slice
+    // On veut 2 clubs au-dessus (index - 2)
+    const startIndex = Math.max(0, lorientIndex - 2); 
+    // On veut 2 clubs en dessous (index + 2), plus Lorient lui-même (index + 1), donc jusqu'à index + 3 (exclusif)
+    const endIndex = Math.min(this.classementLigue1.length, lorientIndex + 3);
+
+    // 3. Extraire le segment du classement
+    return this.classementLigue1.slice(startIndex, endIndex);
+  }
+
   // Pagination
   currentPage = 1;
-  articlesPerPage = 9;
+  articlesPerPage = 4;
 
   get totalPages(): number {
     return Math.ceil(this.articles.length / this.articlesPerPage);
