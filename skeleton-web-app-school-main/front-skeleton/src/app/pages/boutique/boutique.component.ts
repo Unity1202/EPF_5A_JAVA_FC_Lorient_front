41f2032b3bf27999720d1168491d2196f3cdc875
quentindhr <124066086+quentindhr@users.noreply.github.com>
2025-10-13 11:51:20 +0200

    Import joueur

--- skeleton-web-app-school-main/front-skeleton/src/app/pages/boutique/boutique.component.ts
+++ skeleton-web-app-school-main/front-skeleton/src/app/pages/boutique/boutique.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 interface Produit {
-  nom: string;
-  prix: number;
-  image: string;
+  id: number;
+  name: string;
   description: string;
+  image: string;
+  price: number;
 }
+
 
 @Component({
   selector: 'app-boutique',
   templateUrl: './boutique.component.html',
   styleUrls: ['./boutique.component.scss']
 })
-export class BoutiqueComponent {
-  produits: Produit[] = [
-    { nom: 'Maillot Domicile 2025', prix: 79.99, image: 'assets/maillot.png', description: 'Maillot officiel du FC Lorient 2025.' },
-    { nom: 'Écharpe du Club', prix: 24.99, image: 'assets/echarpe.png', description: 'Écharpe orange et noire, symbole de fierté.' },
-    { nom: 'Casquette Officielle', prix: 19.99, image: 'assets/casquette.png', description: 'Casquette brodée avec le logo du club.' }
-  ];
+
+export class BoutiqueComponent implements OnInit {
+  produits: Produit[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchProduits();
+  }
+
+  fetchProduits(): void {
+    this.http.get<Produit[]>('http://localhost:8080/stores').subscribe(data => {
+      console.log('Produits reçus:', data);
+      this.produits = data;
+    });
+  }
+  
 }
